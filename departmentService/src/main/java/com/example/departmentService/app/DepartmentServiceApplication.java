98ca2500dda1a62d01a988e921685358e82e98db
Shubhanjan <rockymountainsandbeaches@gmail.com>
2025-06-12 23:53:31 +0530

    add files

--- departmentService/src/main/java/com/example/departmentService/app/DepartmentServiceApplication.java
+++ departmentService/src/main/java/com/example/departmentService/app/DepartmentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.departmentService;
+package com.example.departmentService.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
